4d467a0e9ba27ede4acf84b2fc582874d49e65a2
Pabitra Banerjee <rockstarpabitra2204@gmail.com>
2023-04-25 09:53:13 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'https://zerooctave.co/ZeroOctave-Javascript-Projects/assets/Audio/sound.mp3'); 
+audio.loop=true;
+audio.play();
